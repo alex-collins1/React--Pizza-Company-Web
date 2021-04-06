f498c1fbe7261fba158898c053285a5cdced6a15
alex-collins1 <73031869+alex-collins1@users.noreply.github.com>
2021-04-06 20:58:59 +0100

    first commit

---
+++ src/components/Feature/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import {FeatureContainer, FeatureButton} from './FeatureElements';
+
+const Feature = () => {
+    return (
+<FeatureContainer>
+<h1>Pizza of the Day</h1>
+<p>BBQ Pizza topped with onions and chicken, with stuffed crust filled with melted cheese </p>
+<FeatureButton>Order Here</FeatureButton>
+
+</FeatureContainer>
+    )
+}
+
+export default Feature
